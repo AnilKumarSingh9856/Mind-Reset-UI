eb809cd96a12cebf85b7074b9e9ff20505d80d6a
AnilKumarSingh9856 <anil.21scse1010654@galgotiasuniversity.edu.in>
2024-09-04 23:53:54 +0530

    commit

---
+++ app/src/main/java/com/example/mindreset/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.mindreset
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.example.mindreset.ui.theme.HomeScreen
+import com.example.mindreset.ui.theme.MindResetTheme
+
+@OptIn(ExperimentalFoundationApi::class)
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MindResetTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    HomeScreen()
+                }
+            }
+        }
+    }
+}
+
